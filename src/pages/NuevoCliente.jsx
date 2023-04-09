794bd7886e228b2b164e6366aea78543afb187b3
German Mahecha <gmahecha@gmail.com>
2023-04-08 22:27:23 -0400

    Action form with react-router-dom

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,14 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Form } from "react-router-dom"
+import Formulario from "../components/Formulario.jsx";
+
+export async function action({request}){
+    const formData = await request.formData()
+
+    const datos = Object.fromEntries(formData)
+    console.log(datos)
+
+    return ("ff")
+}
 function NuevoCliente() {
     const navigate = useNavigate()
     return(
@@ -11,8 +21,18 @@ function NuevoCliente() {
                     onClick={()=>navigate('/')}
                 >Volver</button>
             </div>
-            <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-                <p>Formulario aqui</p>
+            <div className="bg-white shadow mt-20 rounded-md md:w-3/4 mx-auto px-5 py-10">
+                <Form
+                    method="POST"
+
+                >
+                    <Formulario />
+                     <input
+                        type="submit"
+                        className="mt-5 w-full rounded-md bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+                        value="Registar cliente"
+                    />
+                </Form>
             </div>
 
         </>
